22f298c5733e2d4e8f5feeb5f7715fd10b833a6d
Andreea <oanaandreeailiuta@gmail.com>
2020-11-12 18:10:16 +0200

    add email and pasword input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TextInputComponent } from './text-input.component/text-input.component';
 import { LoginComponent} from './login.component/login.component';
-import { LoginContainerComponent} from './login-container.component/login-container.component'
+import { LoginContainerComponent} from './login-container.component/login-container.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 // import { from } from 'rxjs';
 
 @NgModule({
@@ -18,7 +20,9 @@ import { LoginContainerComponent} from './login-container.component/login-contai
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FontAwesomeModule
   ],
 
 
--- src/app/text-input.component/text-input.component.ts
+++ src/app/text-input.component/text-input.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
 	selector: 'text-input',
 	templateUrl: 'text-input.component.html',
-	// styleUrls: ['./text-input.component.css']
+	styleUrls: ['./text-input.component.css']
 })
 
 export class TextInputComponent implements OnInit {
 
 	ngOnInit() { }
-}
+	faEnvelope = faEnvelope;
+	faLock = faLock;
+	@Input() placeholder: string;
+	@Input() inputIcon: string;
+}	
